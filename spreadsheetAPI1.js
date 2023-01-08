88ed73d67ed26f8665183ac609d77ae358d59052
Michael Becker <51341849+21beckem@users.noreply.github.com>
2023-01-08 19:05:08 +0100

    Update spreadsheetAPI1.js

--- spreadsheetAPI1.js
+++ spreadsheetAPI1.js
@@ -1,33 +1,40 @@
 class SpreadsheetApp {
-    static change(el, rowI, colI_ORG, val) {
+    static change(rowI) {
         const thisChangedRow = this.rawRes[this.pairedCol[rowI][0]];
-        //console.log(val);
-        //console.log(rowI);
-        let found = false;
-        for (let i = 0; i < this.changedRows.length; i++) {
-            const el = this.changedRows[i];
-            if (el == rowI) {
-                found = true;
-                if (thisChangedRow[colI_ORG] == val) {
-                    // remove from list
-                    this.changedRows.splice(i, 1);
-                } else {
-
-                }
-                // check if it changed back to original
-                // if so remove
-                // if not update existing
+        //console.log(el, rowI, colI_ORG, val, thisChangedRow);
+        let same = true;
+        for (let i = 0; i < Object.keys(this.editable_cols).length; i++) {
+            const ii = Object.keys(this.editable_cols)[i];
+            //console.log(ii);
+            const el = this.editable_cols[ii];
+            //console.log("old:"+thisChangedRow[ii], "new:"+this.getSingleCell(rowI, ii, true));
+            if (thisChangedRow[ii] != this.getSingleCell(rowI, ii, true)) {
+                same = false;
             }
         }
-        if (!found && thisChangedRow[colI_ORG] != val) {//this.getNowCol(colI_ORG)       , thisChangedRow[colI_ORG], el.id]
+        if (same && this.changedRows.includes(rowI)) {
+            this.changedRows.splice(this.changedRows.indexOf(rowI), 1);
+        }else if (!same && !this.changedRows.includes(rowI)) {
             this.changedRows.push(rowI);
         }
+        //console.log(this.changedRows);
 
-
-        // NEW PLAN!!!
-        
-        // based on the current edited rowI, loop through each of the ss.colsToChange  (or make a new array that saves which cols can be edited) and if none of the current values are different than the original, remove the rowI from the changed cols array.
-        // just realized: how do I get the current value of each of these cells...?
+        const table = document.getElementsByClassName('sheetTbl')[0].firstChild;
+        for (let i = 0; i < table.children.length; i++) {
+            const row = table.children[i];
+            if (this.changedRows.includes(i - 1)) {
+                row.classList.add('changedHighlight');
+            } else {
+                row.classList.remove('changedHighlight');
+            }
+        }
+    }
+    static getSingleCell(row, ORGcol, childEl = false) {
+        if (childEl) {
+            return document.getElementById('cellRangeId_' + row + ',' + ORGcol).firstChild.value;
+        } else {
+            return document.getElementById('cellRangeId_' + row + ',' + ORGcol).value;
+        }
     }
     static getNowCol(org) {
         let newNum = org;
@@ -43,9 +50,10 @@ class SpreadsheetApp {
     static pairedCol = Array();
     static changedRows = Array();
     static colsToHide = undefined;
+    static editable_cols = [];
     constructor(prefs = {}) {
         //check prefs
-        const Rprefs = ['url', 'adjust_cols'];
+        const Rprefs = ['url'];
         let p = Object.keys(prefs);
         Rprefs.forEach(e => {
             if (!p.includes(e)) {
@@ -55,15 +63,11 @@ class SpreadsheetApp {
         });
 
         this.prefs = prefs;
-        this.colsToChange = Object.keys(this.prefs.adjust_cols).map(x => parseInt(x));
-        SpreadsheetApp.colsToHide = this.colsToChange.filter((el) => {
-            try {
-                if (this.prefs.adjust_cols[el].hidden) {
-                    return true;
-                }
-            } catch (e) {}
-            return false;
-        });
+        this.prefs.editable_cols = prefs.editable_cols || {};
+        this.prefs.hidden_cols = prefs.hidden_cols || [];
+        this.colsToChange = Object.keys(this.prefs.editable_cols).concat(this.prefs.hidden_cols).map(x => parseInt(x));
+        SpreadsheetApp.colsToHide = this.prefs.hidden_cols;
+        SpreadsheetApp.editable_cols = this.prefs.editable_cols;
     }
     fetch(pgName, range, container_id) {
         fetch(this.prefs.url + '?type=r&range=' + range + '&pgNam=' + pgName)
@@ -99,40 +103,40 @@ class SpreadsheetApp {
             result += '<tr><th>' + this.prefs.header.join('</th><th>') + '</th></tr>';
         }
         for (var i=0; i<arr.length; i++) {
-            result += "<tr>";
+            result += '<tr id="rowRangeId_' + i + '">';
             for (var j=0; j<arr[i].length; j++) {
+                const idStr = 'cellRangeId_' + i + ',' + j;
                 if (this.colsToChange.includes(j)) {
-                    const aj = this.prefs.adjust_cols[j];
-                    result += this.makeAjustedCell(arr[i][j], aj, [i,j]);
+                    if (this.prefs.hidden_cols.includes(j)) {
+                        continue;
+                    }
+                    const aj = this.prefs.editable_cols[j];
+                    result += '<td id="' + idStr + '">' + this.makeAjustedCell(arr[i][j], aj, [i,j]) + '</td>';
+
                 } else {
-                    result += "<td>" + arr[i][j] + "</td>";
+                    result += '<td id="' + idStr + '">' + arr[i][j] + '</td>';
                 }
             }
-            result += "</tr>";
+            result += '</tr>';
         }
-        result += "</table>";
+        result += '</table>';
         return result;
     }
     makeAjustedCell(val, aj, loc) {
         try {
-            if (aj.hidden) {
-                return '';
-            }
-        } catch(e) {}
-        try {
             if (aj.dropdown.options.length > 0) {
-                let output = '<td><select id="dropdown_' + loc.join(',') + '" onchange="SpreadsheetApp.change(this, ' + loc[0] + ', ' + loc[1] + ', this.value)">';
+                let output = '<select id="dropdown_' + loc.join(',') + '" onchange="SpreadsheetApp.change(' + loc[0] + ')">';
                 for (let i = 0; i < aj.dropdown.options.length; i++) {
                     const el = aj.dropdown.options[i];
                     const sel = (val == el) ? ' selected' : '';
                     output += '<option value="' + el + '"' + sel + '>' + el + '</option>';
                 }
-                output += '</select></td>';
+                output += '</select>';
                 return output;
             }
         } catch (e) {}
 
-        return "<td>" + val + "</td>"
+        return val;
     }
     saveChanges() {
         console.log(SpreadsheetApp.changedRows);
